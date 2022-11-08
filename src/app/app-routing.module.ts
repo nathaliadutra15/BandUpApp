b747621f65d0700725918efff34f7b9cfba78492
Nathália Dutra <nathaliad.oliveira@hotmail.com>
2022-11-08 14:03:43 -0300

    Adicionada novas páginas e rotas para o menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchCommunityComponent } from './search-community/search-community.component';
 
 const routes: Routes = [{
   path: '', component: HomeComponent
 },{
   path: 'login', component: LoginComponent
+},{
+  path: 'music-lab', component:MarketplaceComponent
+}, {
+  path:'community', component: SearchCommunityComponent
+}, {
+  path: 'chat', component: ChatComponent
+}, {
+  path: 'profile/:username', component: ProfileComponent
 }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './login/auth.service';
 import { SearchCommunityComponent } from './search-community/search-community.component';
+import { ChatComponent } from './chat/chat.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
@@ -16,7 +19,10 @@ import { SearchCommunityComponent } from './search-community/search-community.co
     AppComponent,
     HomeComponent,
     LoginComponent,
-    SearchCommunityComponent
+    SearchCommunityComponent,
+    ChatComponent,
+    MarketplaceComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
