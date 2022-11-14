f838ee7277d9493e0719079d407ef2913bf0530e
Nathália Dutra <nathaliad.oliveira@hotmail.com>
2022-11-13 22:22:09 -0300

    Verificação de autenticacao + criação de posts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../login/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public musicFile;
+  public picFile;
+  public fileName;
+  public imageFileName;
+  public musicFileName;
+  public postTxt: string;
+  public isShared = false;
+  public midiaType: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private authService: AuthService, private router: Router) {
+    if (!this.authService.getAutenticacao()) {
+      this.router.navigate(['/login']);
+    }
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onChangeMusic(event) {
+    this.midiaType = "music";
+    this.fileName = this.musicFileName.replace(/C:+\\+(\w+)+\\/, " ");
+    this.musicFile = event;
+  }
+
+  onChangePic(event) {
+    this.midiaType = "picture"
+    this.fileName = this.imageFileName.replace(/C:+\\+(\w+)+\\/, " ");
+    this.picFile = event;
+  }
+
+  eviarPost() {
+    try {
+      let objPost = {
+        posts:[
+          {
+            postTxt: this.postTxt,
+            midia: this.midiaType == "music" ? this.musicFile : (this.midiaType == "picture" ? this.picFile : null), 
+            events: [],
+            comments: [],
+            sharings: [],
+            likes: [],
+            savings: [],
+            isShared: this.isShared
+          }
+        ]
+      }
+     
+      /* this.httpClient.patch("http://localhost:3000/post/create/", JSON.stringify(objCadastro), this.options).subscribe((res) => {
+      }, err => {
+        this.userError = err.error.message;
+      });
+      this.userError = undefined; */
+    } catch (error) {
+      
+    }
+      
   }
 
 }
--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -6,17 +6,23 @@ import { IUser } from './usuario';
 })
 export class AuthService {
   private autenticado: boolean = false;
-
+  private usuario: string;
+  
   constructor() { }
 
   logar(userFromUser: IUser, userFromDB: IUser) {
     if (userFromUser.senha === userFromDB.senha &&
       userFromUser.usuario === userFromDB.usuario) {
+      this.usuario = userFromUser.usuario;
       this.autenticado = true;
     }
   }
 
-  getAutenticacao(): boolean{
+  getAutenticacao() {
     return this.autenticado;
+  }
+
+  getUserAutenticado() {
+    return this.usuario;
   }
 }
